df6b609ee1e5bda27124b2e2eefff590c3112b1f
Marcin <marcinzielinski1983@gmail.com>
2020-05-23 09:48:10 +0100

    pierwszy commit na master

---
+++ src/pl/sdacademy/london4/Main.java
@@ -0,0 +1,11 @@
+package pl.sdacademy.london4;
+
+import javax.sound.midi.Soundbank;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("pierwsza zmiana");
+    }
+}
